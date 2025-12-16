2c3602206e9d05087f17f00a134fedffefbded33
logicalPanda2 <TBA>
2025-12-16 14:18:43 +0700

    Add basic features

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,144 @@
+import { useState } from "react";
+
+interface Todo {
+    title: string,
+    details: string,
+    status: string,
+    tag: string,
+    id: string,
+}
+
 export default function App() {
-	return <></>;
+    const [todos, setTodos] = useState<Todo[] | null>(null);
+    const [isModalOpen, setModalOpen] = useState<boolean>(false);
+    const [hasError, setError] = useState<boolean>(false);
+    const [titleValue, setTitleValue] = useState<string>("");
+    const [detailsValue, setDetailsValue] = useState<string>("");
+    const [tagValue, setTagValue] = useState<string>("none");
+    const root = document.getElementById("root");
+
+    const createTodo = (title: string, details: string, tag: string): number => {
+        if(!title) {
+            return 1;
+        }
+
+        const newTodo: Todo = {
+            title: title,
+            details: details,
+            status: "todo",
+            tag: tag,
+            id: crypto.randomUUID(),
+        }
+
+        setTitleValue("");
+        setDetailsValue("");
+        setTagValue("none");
+
+        !todos ? setTodos([newTodo]) : setTodos((prev) => [...prev as any, newTodo]); // fix "as any" later.
+
+        return 0;
+    }
+
+    const deleteTodo = (id: string): void => {
+        if(!todos) return;
+
+        const newTodos = todos.filter(todo => todo.id !== id);
+
+        setTodos(newTodos);
+    }
+
+    const toggleModal = (): void => {
+        if(!root) throw new Error("root element not found");
+
+        const newState = !isModalOpen;
+        setModalOpen(newState);
+        [...root.children].forEach(child => child.toggleAttribute("inert"));
+    }
+
+	return (
+        <>
+            <button 
+                onClick={toggleModal}
+            >
+                Create new task
+            </button>
+            <div>
+                {todos && todos.length > 0
+                    ? todos.map((todo) => (
+                        <div
+                            key={todo.id}
+                        >
+                            <p>{todo.title}</p>
+                            <p>{todo.details}</p>
+                            <p>{todo.tag}</p>
+                            <p>{todo.id}</p>
+                            <button onClick={() => {deleteTodo(todo.id)}}>Delete Task</button>
+                        </div>
+                    ))
+                    : <p>No current tasks</p>
+                }
+            </div>
+            {isModalOpen &&
+                <div
+                    className="flex flex-col w-2xs"
+                >
+                    <label 
+                        htmlFor="titleField"
+                    >
+                        Task Title
+                    </label>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        id="titleField" 
+                        className="border border-solid border-black"
+                        value={titleValue}
+                        onChange={(e) => {setTitleValue(e.target.value)}}
+                    />
+                    <label 
+                        htmlFor="detailsField"
+                    >
+                        Task Details
+                    </label>
+                    <input 
+                        type="text" 
+                        name="details" 
+                        id="detailsField" 
+                        className="border border-solid border-black"
+                        value={detailsValue}
+                        onChange={(e) => {setDetailsValue(e.target.value)}}
+                    />
+                    <label 
+                        htmlFor="tagField"
+                    >
+                        Task Tag
+                    </label>
+                    <select 
+                        name="tag" 
+                        id="tagField"
+                        className="border border-solid border-black"
+                        value={tagValue}
+                        onChange={(e) => {setTagValue(e.target.value)}}
+                    >
+                        <option value="none">None</option>
+                        <option value="low">Low Priority</option>
+                        <option value="mid">Medium Priority</option>
+                        <option value="high">High Priority</option>
+                    </select>
+
+                    <button onClick={() => {
+                        createTodo(titleValue, detailsValue, tagValue) === 1
+                        ? setError(!hasError) // fix incorrect error toggling later.
+                        : toggleModal()
+                    }}>
+                        Create
+                    </button>
+                    <button onClick={toggleModal}>Close</button>
+                    {hasError &&
+                        <p>Cannot create task with no title</p>
+                    }
+                </div>
+            }
+        </>
+    );
 }
