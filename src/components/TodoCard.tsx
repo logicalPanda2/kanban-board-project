84353150cdcaa49f339597e89b4356e2b6704244
Marcel <marcelrg.business@gmail.com>
2025-12-17 18:26:09 +0700

    Add styles

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -19,15 +19,16 @@ export default function TodoCard({
 			ref={(node) => {
 				dragRef(node);
 			}}
-			className="border border-solid border-black rounded-lg p-4"
+			className="rounded-lg px-4 py-3 bg-gray-100 [box-shadow:0_1px_1px_black,inset_0_1px_2px_white]"
 			style={{ opacity: isDragging ? 0.5 : 1 }}
 		>
-			<p className="text-xl mb-2">{todo.title}</p>
+			<p className="text-xl mb-2 text-left px-2 pt-1">{todo.title}</p>
 			<div className="flex flex-row flex-nowrap justify-between items-center">
 				<button
 					onClick={() => {
 						onTaskView(todo);
 					}}
+                    className="text-gray-700 px-2 py-1 rounded-lg focus-visible:bg-gray-300 hover:bg-gray-300 hover:text-black focus-visible:text-black focus-visible:outline-0 transition"
 				>
 					View details...
 				</button>
@@ -35,6 +36,7 @@ export default function TodoCard({
 					onClick={() => {
 						onTaskDelete(todo.id);
 					}}
+                    className="text-red-500 hover:text-red-700 focus-visible:text-red-700 focus-visible:bg-red-200 hover:bg-red-200 px-2 py-1 rounded-lg focus-visible:outline-0 transition"
 				>
 					Delete
 				</button>
