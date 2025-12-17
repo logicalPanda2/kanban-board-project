c9f9e0ca414bab749c339a0417167125aea04a66
Marcel <marcelrg.business@gmail.com>
2025-12-17 17:34:51 +0700

    Add colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,20 +12,20 @@ function isTag(value: string): value is Tag {
 
 export default function Header({ onToggle, value, onChange }: HeaderProps) {
 	return (
-		<header className="flex flex-row flex-nowrap items-center py-4 px-6 justify-between">
-			<h1 className="text-4xl">Kanban Board</h1>
+		<header className="flex flex-row flex-nowrap items-center py-4 px-6 justify-between bg-green-800 text-white">
+			<h1 className="text-4xl font-semibold">Kanban Board</h1>
 			<div className="flex flex-row flex-nowrap items-center">
 				<button
 					onClick={onToggle}
-					className="mx-4 border border-solid border-black py-2 px-4 rounded-lg"
+					className="mx-4 py-2 px-4 rounded-lg bg-green-700/50 [box-shadow:0_1px_2px_black] active:bg-green-900/75 active:[box-shadow:inset_0_0_2px_0_black] active:text-gray-200 font-normal hover:bg-green-900/80 transition focus-visible:[box-shadow:0_3px_4px_rgba(0,0,0,0.75)] focus-visible:outline-0"
 				>
 					Create new task
 				</button>
 				<div className="flex flex-col ml-8">
-					<label htmlFor="filter" className="mb-1">
+					<label htmlFor="filter" className="mb-1 font-medium">
 						Filter
 					</label>
-					<div className="border border-solid border-black py-1 px-2 rounded-lg focus-within:outline-[1.5px]">
+					<div className="py-1 px-2 rounded-lg active:bg-green-900/75 active:[box-shadow:inset_0_0_2px_0_black] active:text-gray-200 bg-green-700/50 [box-shadow:0_1px_2px_black] font-normal hover:bg-green-900/80 transition focus-within:[box-shadow:0_3px_4px_rgba(0,0,0,0.75)]">
 						<select
 							name="filter"
 							id="filter"
@@ -35,12 +35,12 @@ export default function Header({ onToggle, value, onChange }: HeaderProps) {
 									? onChange(e.target.value)
 									: onChange("none");
 							}}
-							className="focus-visible:outline-0 "
+							className="focus-visible:outline-0"
 						>
-							<option value="none">None</option>
-							<option value="low">Low Priority</option>
-							<option value="mid">Medium Priority</option>
-							<option value="high">High Priority</option>
+							<option value="none" className="text-black">None</option>
+							<option value="low" className="text-black">Low Priority</option>
+							<option value="mid" className="text-black">Medium Priority</option>
+							<option value="high" className="text-black">High Priority</option>
 						</select>
 					</div>
 				</div>
