d13a6adf2b33322fe70ab8cc0140257897a47f49
logicalPanda2 <TBA>
2025-12-17 20:21:25 +0700

    Add modal transition

--- src/components/TodoModal.tsx
+++ src/components/TodoModal.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 function isTag(value: string): value is Tag {
 	if (
 		value === "none" ||
@@ -24,9 +26,29 @@ export default function TodoModal({
 	onCreate,
 	onClose,
 }: ModalProps) {
+    const overlayRef = useRef<HTMLDivElement | null>(null);
+    const modalRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if(!modalRef.current || !overlayRef.current) return;
+
+        overlayRef.current.classList.add("bg-transparent");
+        modalRef.current.classList.add("top-128");
+        let timeout = setTimeout(() => {
+            if(!modalRef.current || !overlayRef.current) return;
+
+            overlayRef.current.classList.remove("bg-transparent");
+            modalRef.current.classList.remove("top-128");
+        }, 4);
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    })
+
 	return (
-		<div className="fixed inset-0 z-20 bg-neutral-600/70 flex items-center justify-center">
-			<div className="w-lg h-96 rounded-2xl px-6 py-4 bg-gray-100 flex flex-col relative [box-shadow:0_1px_2px_black,inset_0_1px_2px_white] selection:bg-green-800 selection:text-neutral-100">
+		<div className="fixed inset-0 z-20 bg-neutral-600/70 flex items-center justify-center transition" ref={overlayRef}>
+			<div className="w-lg h-96 rounded-2xl px-6 py-4 bg-gray-100 flex flex-col relative [box-shadow:0_1px_2px_black,inset_0_1px_2px_white] selection:bg-green-800 selection:text-neutral-100 [transition:top_250ms_ease] top-0" ref={modalRef}>
 				<p className="text-3xl mb-4">
 					{editedId ? "Edit" : "New"}
 				</p>
