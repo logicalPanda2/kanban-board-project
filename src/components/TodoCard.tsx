de90572fa6ac247eb538bcb25e3bba0484bf8564
Marcel <marcelrg.business@gmail.com>
2025-12-17 18:45:43 +0700

    Add visual tag indicator

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -19,10 +19,10 @@ export default function TodoCard({
 			ref={(node) => {
 				dragRef(node);
 			}}
-			className="rounded-lg px-4 py-3 bg-gray-100 [box-shadow:0_1px_1.5px_black,inset_0_1px_2px_white]"
+			className="rounded-lg px-4 py-3 bg-gray-100 [box-shadow:0_1px_1.5px_black,inset_0_1px_2px_white] relative"
 			style={{ opacity: isDragging ? 0.5 : 1 }}
 		>
-			<p className="text-xl mb-2 text-left px-2 pt-1">{todo.title}</p>
+			<p className="text-xl mb-2 text-left px-2 pt-1  wrap-break-word">{todo.title}</p>
 			<div className="flex flex-row flex-nowrap justify-between items-center">
 				<button
 					onClick={() => {
@@ -40,6 +40,19 @@ export default function TodoCard({
 				>
 					Delete
 				</button>
+                <div className={`absolute w-3 h-3 rounded-full top-5 right-6 border border-solid border-gray-800
+                    ${
+                        todo.tag === "none"
+                        ? "bg-white"
+                        : todo.tag === "low"
+                        ? "bg-green-500"
+                        : todo.tag === "mid"
+                        ? "bg-amber-500"
+                        : todo.tag === "high"
+                        ? "bg-red-500"
+                        : "bg-white"
+                    }`}>    
+                </div>
 			</div>
 		</div>
 	);
