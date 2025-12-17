8bf53a0c0f34ee6a98a76605dbe2a48eadad385a
Marcel <marcelrg.business@gmail.com>
2025-12-17 16:16:00 +0700

    Add filter back in

--- src/App.tsx
+++ src/App.tsx
@@ -60,10 +60,31 @@ export default function App() {
 	return (
 		<>
 			<header className="flex flex-row flex-nowrap items-center py-4 px-6 justify-between">
+                <h1 className="text-4xl">Kanban Board</h1>
                 <div className="flex flex-row flex-nowrap items-center">
-                    <h1 className="text-4xl">Kanban Board</h1>
+                    <button onClick={toggleModal} className="mx-4 border border-solid border-black py-2 px-4 rounded-lg">Create new task</button>
+                    <div className="flex flex-col ml-8">
+                        <label htmlFor="filter" className="mb-1">Filter</label>
+                        <div className="border border-solid border-black py-1 px-2 rounded-lg focus-within:outline-[1.5px]">
+                            <select
+                                name="filter"
+                                id="filter"
+                                value={filterValue}
+                                onChange={(e) => {
+                                    isTag(e.target.value)
+                                        ? setFilterValue(e.target.value)
+                                        : setFilterValue("none");
+                                }}
+                                className="focus-visible:outline-0 "
+                            >
+                                <option value="none">None</option>
+                                <option value="low">Low Priority</option>
+                                <option value="mid">Medium Priority</option>
+                                <option value="high">High Priority</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-                <button onClick={toggleModal} className="mx-4 border border-solid border-black py-2 px-4 rounded-lg">Create new task</button>
 			</header>
             <DndProvider backend={HTML5Backend}>
 				<main className="flex flex-col grow">
