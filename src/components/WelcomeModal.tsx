95eb471f2ddf60b819ff76eb5200efc02caa527d
Marcel <marcelrg.business@gmail.com>
2025-12-18 13:48:52 +0700

    Remove modal transitions

--- src/components/WelcomeModal.tsx
+++ src/components/WelcomeModal.tsx
@@ -1,34 +1,12 @@
-import { useRef, useEffect } from "react";
-
 export default function WelcomeModal({onClose}: WelcomeModalProps) {
-    const overlayRef = useRef<HTMLDivElement | null>(null);
-    const modalRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-        if(!modalRef.current || !overlayRef.current) return;
-
-        overlayRef.current.classList.add("bg-transparent");
-        modalRef.current.classList.add("top-512");
-        let timeout = setTimeout(() => {
-            if(!modalRef.current || !overlayRef.current) return;
-
-            overlayRef.current.classList.remove("bg-transparent");
-            modalRef.current.classList.remove("top-512");
-        }, 4);
-
-        return () => {
-            clearTimeout(timeout);
-        }
-    }, [modalRef, overlayRef])
-
     return (
-        <div className="fixed inset-0 z-20 bg-neutral-600/70 flex items-center justify-center transition" ref={overlayRef}>
-			<div className="w-lg h-96 rounded-2xl px-6 py-4 bg-gray-100 flex flex-col relative [box-shadow:0_1px_2px_black,inset_0_1px_2px_white] selection:bg-green-800 selection:text-neutral-100 [transition:top_250ms_ease] top-0 mx-4" ref={modalRef}>
+        <div className="fixed inset-0 z-20 bg-neutral-600/70 flex items-center justify-center transition">
+			<div className="w-lg h-96 rounded-2xl px-6 py-4 bg-gray-100 flex flex-col relative [box-shadow:0_1px_2px_black,inset_0_1px_2px_white] selection:bg-green-800 selection:text-neutral-100 [transition:top_250ms_ease] top-0 mx-4">
                 <p className="text-3xl text-center">Welcome!</p>
                 <ul className="list-disc mx-4 mt-4">
                     <li className="mb-2">Make and track your tasks easily with this kanban-style task tracker.</li>
                     <li className="mb-2">To change a task's status, use the arrows in the bottom of each task. You can also drag and drop tasks in desktop.</li>
-                    <li className="mb-2">The tags 
+                    <li className="mb-2">For flexibility, The tags 
                         <div className="rounded-full inline-block w-3 h-3 border border-solid border-gray-800 bg-white mx-1"></div>
                         <div className="rounded-full inline-block w-3 h-3 border border-solid border-gray-800 bg-green-500 mx-1"></div>
                         <div className="rounded-full inline-block w-3 h-3 border border-solid border-gray-800 bg-amber-500 mx-1"></div>
