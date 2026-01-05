bb925599501507a53220ea6235540672c5ccbaa3
Marcel <marcelrg.business@gmail.com>
2026-01-05 17:28:14 +0700

    Add test plans

---
+++ src/hooks/useTodos.test.tsx
@@ -0,0 +1,12 @@
+describe("useTodos hook", () => {
+    // NOT: test if it creates an empty array
+    // NOT: test if it returns existing todos
+    // 1: check if create todo throws without a title
+    // 2: check if create todo returns true
+    // 4: check if edit todo throws without a title
+    // 5: check if edit todo throws when it doesn't find oldTodo (meaning: the targetId provided doesn't refer to any existing Todos)
+    // 6: check if edit todo returns true
+    // 7: check if edit todo properly replaces the old todo with the new one
+    // 8: check if delete todo properly deletes a todo 
+    // 9 - 12: check all 4 branches in changeTodoStatus
+});
