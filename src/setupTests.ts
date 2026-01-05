c4575555ab791993c65dd2645f42a5f912256d6f
Marcel <marcelrg.business@gmail.com>
2026-01-05 16:57:16 +0700

    Add mock local storage

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,24 @@
 import "@testing-library/jest-dom/vitest";
+
+const localStorageMock = (() => {
+    let storage: Record<string, string> = {}
+
+    return {
+        getItem: (key: string) => (
+            storage[key] ?? null
+        ),
+        setItem: (key: string, value: string) => {
+            storage[key] = value;
+        },
+        removeItem: (key: string) => {
+            delete storage[key];
+        },
+        clear: () => {
+            storage = {};
+        },
+    }
+})();
+
+Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+});
