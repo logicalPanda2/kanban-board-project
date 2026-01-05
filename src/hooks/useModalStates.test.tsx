236d75df7dbe8d6416fa63a2abf108a17992ccef
Marcel <marcelrg.business@gmail.com>
2026-01-05 20:35:05 +0700

    Add tests

--- src/hooks/useModalStates.test.tsx
+++ src/hooks/useModalStates.test.tsx
@@ -1,3 +1,56 @@
+import { renderHook } from "@testing-library/react";
+import { useModalStates } from "./useModalStates";
+import { act } from "react";
+
 describe("useModalStates hook", () => {
-    // refactor useModalStates later. logic is heavily coupled with the DOM right now
-})
+    it("resets all states when opening the modal", () => {
+        const {result} = renderHook(() => (
+            useModalStates()
+        ));
+
+        result.current.openModal();
+
+        expect(result.current.titleValue).toBe("");
+        expect(result.current.detailsValue).toBe("");
+        expect(result.current.tagValue).toBe("none");
+        expect(result.current.editedId).toBe("");
+        expect(result.current.hasError).toBe(false);
+    });
+
+    it("resets all states when closing the modal", () => {
+        const {result} = renderHook(() => (
+            useModalStates()
+        ));
+
+        result.current.closeModal();
+
+        expect(result.current.titleValue).toBe("");
+        expect(result.current.detailsValue).toBe("");
+        expect(result.current.tagValue).toBe("none");
+        expect(result.current.editedId).toBe("");
+        expect(result.current.hasError).toBe(false);
+    });
+
+    it("will apply necessary states when viewing details", () => {
+        const {result} = renderHook(() => (
+            useModalStates()
+        ));
+
+        const mockTodo: Todo = {
+            title: "new todo",
+            details: "not empty",
+            tag: "low",
+            status: "todo",
+            id: crypto.randomUUID(),
+        };
+
+        act(() => {
+            result.current.viewDetails(mockTodo);
+        })
+
+        expect(result.current.titleValue).toBe(mockTodo.title);
+        expect(result.current.detailsValue).toBe(mockTodo.details);
+        expect(result.current.tagValue).toBe(mockTodo.tag);
+        expect(result.current.editedId).toBe(mockTodo.id);
+    });
+});
