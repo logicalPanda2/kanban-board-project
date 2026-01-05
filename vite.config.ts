d46cc54816b21eacb95759948d503c491f01e366
Marcel <marcelrg.business@gmail.com>
2026-01-05 16:06:14 +0700

    Configure testing env

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({ plugins: [react(), tailwindcss()] });
+export default defineConfig({ 
+    plugins: [react(), tailwindcss()],
+    test: {
+        environment: "jsdom",
+        setupFiles: "./src/setupTests.ts",
+        globals: true,
+    }
+});
