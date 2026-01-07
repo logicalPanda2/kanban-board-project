2d443a5e5dc25e352eb329b079722bf43e335693
Marcel <marcelrg.business@gmail.com>
2026-01-08 00:45:20 +0700

    Add test file

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+describe("App", () => {
+    it("displays the create/edit modal");
+    it("displays the welcome modal");
+    it("sets inert on other elements if a modal is open");
+    it("removes inert when a modal is closed");
+    it("filters todos based on the filter value");
+    it("provides the correct todos for each column based on status");
+})
