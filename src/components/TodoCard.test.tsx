32d99e0ff6a5f300308bd19f664711ddc9e0bbc7
Marcel <marcelrg.business@gmail.com>
2026-01-08 00:03:41 +0700

    Add test file

---
+++ src/components/TodoCard.test.tsx
@@ -0,0 +1,7 @@
+describe("TodoCard", () => {
+    it("renders a todo's information");
+    it("calls changeTodoStatus with a 'left' key when the left button is clicked");
+    it("calls changeTodoStatus with a 'right' key when the right button is clicked");
+    it("is accessible through aria labels");
+    it("displays the correct tag");
+})
