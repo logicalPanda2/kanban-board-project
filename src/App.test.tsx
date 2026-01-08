6c3a41288c7c07fb90314b16e0adbbeffc0a75eb
Marcel <marcelrg.business@gmail.com>
2026-01-08 18:52:59 +0700

    Add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,75 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
+
 describe("App", () => {
-    it("displays the create/edit modal");
-    it("displays the welcome modal");
-    it("sets inert on other elements if a modal is open");
-    it("removes inert when a modal is closed");
-    it("filters todos based on the filter value");
-    it("provides the correct todos for each column based on status");
+    beforeEach(() => {
+        localStorage.clear();
+    });
+
+    it("displays the welcome modal on first visit", () => {
+        const root = document.createElement("div");
+        root.id = "root";
+        document.body.appendChild(root);
+
+        render(<App />, {container: root});
+
+        expect(screen.getByText("Welcome!")).toBeInTheDocument();
+    });
+
+    it("displays the create/edit modal", async () => {
+        const user = userEvent.setup();
+        const root = document.createElement("div");
+        root.id = "root";
+        document.body.appendChild(root);
+
+        render(<App />, {container: root});
+
+        await user.click(screen.getByText("New task"));
+
+        expect(screen.getByText("New")).toBeInTheDocument();
+    });
+
+    it("sets inert on other elements if the welcome modal is open", () => {
+        const root = document.createElement("div");
+        root.id = "root";
+        document.body.appendChild(root);
+
+        render(<App />, {container: root});
+
+        const inertElements = [...root.children].filter(el => el.hasAttribute("inert"));
+
+        expect(inertElements.length).toBe(2); // header + main
+    });
+
+    it("sets inert on other elements if the create/edit modal is open", async () => {
+        const user = userEvent.setup();
+        const root = document.createElement("div");
+        root.id = "root";
+        document.body.appendChild(root);
+
+        render(<App />, {container: root});
+
+        await user.click(screen.getByText("X"));
+        await user.click(screen.getByText("New task"));
+
+        const inertElements = [...root.children].filter(el => el.hasAttribute("inert"));
+
+        expect(inertElements.length).toBe(2); // header + main
+    });
+
+    it("removes inert when a modal is closed", async () => {
+        const user = userEvent.setup();
+        const root = document.createElement("div");
+        root.id = "root";
+        document.body.appendChild(root);
+
+        render(<App />, {container: root});
+
+        await user.click(screen.getByText("X"));
+
+        const inertElements = [...root.children].filter(el => el.hasAttribute("inert"));
+
+        expect(inertElements.length).toBe(0);
+    });
 })
