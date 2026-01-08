8f9ae0fe0c7fa9b8564940689e3ec1e9b5f06e4f
Marcel <marcelrg.business@gmail.com>
2026-01-08 15:22:05 +0700

    Add tests

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,5 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { vi } from "vitest";
+import Header from "./Header";
+import { useState } from "react";
+
+function FakeHeader() {
+    const [value, setValue] = useState<Tag>("none");
+
+    return (
+        <Header
+            onToggle={vi.fn()}
+            value={value}
+            onChange={setValue}
+        />
+    );
+}
+
 describe("Header", () => {
-    it("toggles the create new modal");
-    it("displays a filter value from a given prop");
-    it("changes the filter value on change");
+    it("toggles the create new modal", async () => {
+        const user = userEvent.setup();
+        const onToggle = vi.fn();
+
+        render(
+            <Header 
+                onToggle={onToggle} 
+                value="none"
+                onChange={vi.fn()} 
+            />
+        );
+
+        await user.click(screen.getByRole("button"));
+
+        expect(onToggle).toHaveBeenCalled();
+    });
+
+    it("displays a filter value from a given prop", () => {
+        render(
+            <Header 
+                onToggle={vi.fn()} 
+                value="none"
+                onChange={vi.fn()} 
+            />
+        );
+
+        expect(screen.getByRole("combobox")).toHaveValue("none");
+    });
+
+    it("changes the filter value", async () => {
+        const user = userEvent.setup();
+
+        render(<FakeHeader />);
+
+        await user.selectOptions(screen.getByRole("combobox"), "High");
+
+        expect(screen.getByRole("combobox")).toHaveValue("high");
+    });
 })
