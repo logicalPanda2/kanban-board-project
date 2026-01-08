dd3000b29095a7d8efb70a10004551df9b299629
Marcel <marcelrg.business@gmail.com>
2026-01-08 16:14:11 +0700

    Add test

--- src/components/WelcomeModal.test.tsx
+++ src/components/WelcomeModal.test.tsx
@@ -1,3 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import WelcomeModal from "./WelcomeModal";
+import userEvent from "@testing-library/user-event";
+
 describe("WelcomeModal", () => {
-    it("closes the modal");
+    it("closes the modal", async () => {
+        const onClose = vi.fn();
+        const user = userEvent.setup();
+
+        render(<WelcomeModal onClose={onClose} />);
+
+        await user.click(screen.getByRole("button"));
+
+        expect(onClose).toHaveBeenCalled();
+    });
 })
